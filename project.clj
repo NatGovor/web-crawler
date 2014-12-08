2885a711d350458fe70024fa7e0243bdaa14b369
Natalia Govor <ngovor93@gmail.com>
2014-12-08 16:27:50 +0300

    Added file with urls, started parsing gerring html from links

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [enlive "1.1.5"]]
+                 [enlive "1.1.5"]
+                 [clj-http "1.0.1"]
+                 [slingshot "0.12.1"]]
   :main ^:skip-aot web-crawler.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/web_crawler/core.clj
+++ src/web_crawler/core.clj
@@ -1,13 +1,51 @@
 (ns web-crawler.core
   (:gen-class)
   (:require [net.cgrand.enlive-html :as html])
-  (:require [clojure.string :as string]))
+  (:require [clojure.java.io :as io])
+  (:require [clojure.string :as string])
+  (:require [clj-http.client :as client])
+  (:use [slingshot.slingshot :only [try+]]))
+
+(defn read-file
+  [file-name]
+  (with-open [rdr (io/reader file-name)]
+    (doall (line-seq rdr))))
+
+(defn fetch-html
+  [url]
+  (println "try fetch")
+  (try+
+   (client/get url {:throw-exceptions false})
+   (catch Object _ {:status 404 :headers nil})))
+
+(defn parse-html-page
+  [url]
+  (let [html (fetch-html url)]
+    (println html)))
+
+(defn visit-node
+  [urls depth]
+  (let [new-depth (dec depth)]
+    ;(if (< depth 1)
+    ;  (println new-depth)
+    ;  (println new-depth))))
+    ;(map #(parse-html-page url) urls)))
+    (parse-html-page (first urls))))
+
+(defn crawling
+  [file-name depth]
+  (let [urls (read-file file-name)]
+    (visit-node urls depth)))
 
 (defn -main
   [& args]
-  (let
-    [links (map :href
-       (map :attrs
-             (html/select (html/html-resource (java.net.URL. "http://clojure-doc.org/articles/tutorials/introduction.html")) #{[:a]})))
-     ]
-    (println (string/join "\n" links))))
+  (let [file-name "resources/urls.txt"
+        depth 1]
+    (crawling file-name depth)))
+  ;(let
+  ;  [links (map :href
+  ;     (map :attrs
+  ;           (html/select (html/html-resource (java.net.URL. "https://github.com/")) #{[:a]})))
+  ;   ]
+  ;  (println (string/join "\n" links))))
+
